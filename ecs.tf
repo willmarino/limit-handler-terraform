905bfdb0f8ea5399a775b200250e670de833e982
wmarino <w.marino997@gmail.com>
2024-04-12 21:44:46 -0400

    retry

--- ecs.tf
+++ ecs.tf
@@ -220,20 +220,3 @@ resource "aws_ecs_service" "limit_handler" {
     container_port   = 5050
   }
 }
-
-locals {
-  datadog_environment = {
-    DD_API_KEY = var.datadog_api_key
-    DD_SITE    = var.datadog_site
-
-    DD_APM_ENABLED                 = "true"
-    DD_DOGSTATSD_NON_LOCAL_TRAFFIC = "true"
-    DD_APM_NON_LOCAL_TRAFFIC       = "true"
-    DD_PROCESS_AGENT_ENABLED       = "true"
-    DD_LOGS_ENABLED                = "true"
-
-    DD_LOGS_CONFIG_CONTAINER_COLLECT_ALL = "true"
-
-    ECS_FARGATE = "true"
-  }
-}
