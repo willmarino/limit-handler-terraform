5364b396cf77dcf3e7c36cb9515bd5d4cb1114ac
wmarino <w.marino997@gmail.com>
2024-04-12 21:28:28 -0400

    getting rid of ignore_changes block

--- ecs.tf
+++ ecs.tf
@@ -174,7 +174,8 @@ resource "aws_ecs_task_definition" "limit_handler" {
   }])
 
   lifecycle {
-    ignore_changes = [container_definitions]
+    # ignore_changes = [container_definitions]
+    ignore_changes = []
   }
 }
 
